2ccd8db7d09b482b6a39d5a17278260f0c3bcc0f
Jonathan Bouganim <jonathanbouganim@gmail.com>
2025-02-04 16:55:41 -0500

    TST Customizations

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/order-summary/order-summary-item.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/order-summary/order-summary-item.tsx
@@ -138,6 +138,12 @@ const OrderSummaryItem = ( {
 				fullDescription,
 		  };
 
+	// 86co start
+	// 86inc 86co lumise data photo
+	const customImages = extensions?.lumise_data?.screenshots || [];
+	const firstImage = customImages.length ? customImages[0] : images[0];
+	// 86co end
+
 	return (
 		<div
 			className={ clsx(
@@ -161,10 +167,12 @@ const OrderSummaryItem = ( {
 						) }
 					/>
 				</div>
+				{ /* 86co start */ }
 				<ProductImage
-					image={ images.length ? images[ 0 ] : {} }
+					image={ firstImage }
 					fallbackAlt={ name }
 				/>
+				{ /* 86co end */ }
 			</div>
 			<div className="wc-block-components-order-summary-item__description">
 				<ProductName
--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
@@ -429,6 +429,10 @@ class MiniCart extends AbstractBlock {
 			$show_product_count = $cart && $cart->get_cart_contents_count() > 0;
 		}
 
+		// 86co start
+		$icon = apply_filters('woocommerce_mini_cart_icon', $icon, $icon_color );
+		// 86co end
+
 		$button_html = $this->get_cart_price_markup( $attributes ) . '
 		<span class="wc-block-mini-cart__quantity-badge">
 			' . $icon . '
